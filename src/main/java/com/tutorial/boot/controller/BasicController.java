4676c29d37197ff559c24407c4c9802903acbc70
hodong.kim <fship1124@gmail.com>
2019-08-04 23:58:07 +0900

    PathVariable

--- src/main/java/com/tutorial/boot/controller/BasicController.java
+++ src/main/java/com/tutorial/boot/controller/BasicController.java
@@ -3,11 +3,10 @@ package com.tutorial.boot.controller;
 import com.tutorial.boot.model.Todo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -28,4 +27,19 @@ public class BasicController {
     public ResponseEntity<Todo> postBasicR(@RequestParam(value = "todoTitle") String todoTitle) {
         return new ResponseEntity(new Todo(counter.incrementAndGet(), todoTitle), HttpStatus.CREATED);
     }
+
+    @RequestMapping(value = "/todos/{todoId}")
+    public Todo getPath(@PathVariable int todoId) {
+        Todo t1 = new Todo(1L, "문서쓰기");
+        Todo t2 = new Todo(1L, "문서쓰기2");
+        Todo t3 = new Todo(1L, "문서쓰기3");
+
+        Map<Integer, Todo> todoMap = new HashMap<>();
+        todoMap.put(1, t1);
+        todoMap.put(2, t2);
+        todoMap.put(3, t3);
+
+        return todoMap.get(todoId);
+    }
+
 }
