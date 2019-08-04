7cf6ca82a1c1fa7d22fb886eb8395bdd702603b4
hodong.kim <fship1124@gmail.com>
2019-08-04 23:51:49 +0900

    응답헤더 활용 - ResponseEntity

--- src/main/java/com/tutorial/boot/controller/BasicController.java
+++ src/main/java/com/tutorial/boot/controller/BasicController.java
@@ -1,7 +1,11 @@
 package com.tutorial.boot.controller;
 
 import com.tutorial.boot.model.Todo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,5 +19,13 @@ public class BasicController {
         return new Todo(counter.incrementAndGet(), "라면사오기");
     }
 
+    @RequestMapping(value = "/todop", method = RequestMethod.POST)
+    public Todo postBasic(@RequestParam(value = "todoTitle") String todoTitle) {
+        return new Todo(counter.incrementAndGet(), todoTitle);
+    }
 
+    @RequestMapping(value = "/todor", method = RequestMethod.POST)
+    public ResponseEntity<Todo> postBasicR(@RequestParam(value = "todoTitle") String todoTitle) {
+        return new ResponseEntity(new Todo(counter.incrementAndGet(), todoTitle), HttpStatus.CREATED);
+    }
 }
