62aef4f857dd2af58652c77e9dcd44018aa5efe2
hodong.kim <fship1124@gmail.com>
2019-08-13 00:30:52 +0900

    file upload

--- src/main/java/com/tutorial/boot/controller/BasicController.java
+++ src/main/java/com/tutorial/boot/controller/BasicController.java
@@ -4,7 +4,9 @@ import com.tutorial.boot.model.Todo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -14,8 +16,20 @@ public class BasicController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/todo")
+    @CrossOrigin
     public Todo basic() {
+
         return new Todo(counter.incrementAndGet(), "라면사오기");
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = "text/plain;charset=utf-8")
+    @CrossOrigin
+    public ResponseEntity<?> upload(@RequestParam("extraField") String extraField, @RequestParam("file") MultipartFile[] files) {
+        System.out.println(extraField);
+        System.out.println(files.length);
+
+        //return new Todo(counter.incrementAndGet(), "라면사오기");
+        return new ResponseEntity("Successfully uploaded - " + extraField, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/todop", method = RequestMethod.POST)
