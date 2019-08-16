7a864f8a4e5490b0b972dac6a3affeccbac42146
hodong.kim <hodong.kim@lemonhc.com>
2019-08-16 12:05:11 +0900

    vue start

---
+++ src/main/java/com/tutorial/boot/controller/VueController.java
@@ -0,0 +1,17 @@
+package com.tutorial.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("vue")
+public class VueController {
+
+    @RequestMapping(value = "/start")
+    public String start(Model model) {
+        model.addAttribute("message", "hello boot");
+
+        return "vue/start";
+    }
+}
